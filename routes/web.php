86e745738f07404fed9936bc07396d94e9f53022
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-10-14 12:33:20 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExampleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [ExampleController::class, 'homepage']);
+
+Route::get('/about', [ExampleController::class, 'about']);
