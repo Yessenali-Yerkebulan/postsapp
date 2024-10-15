bdca8a35c13f28cd7abf88636f240d86a51e7ece
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-10-15 19:00:36 +0500

    add pagination for feed posts at homepage

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
 {
     public function showCorrectHomepage() {
         if(auth()->check()) {
-            return view('homepage-feed', ['posts'=>auth()->user()->feedPosts()->latest()->get()]);
+            return view('homepage-feed', ['posts'=>auth()->user()->feedPosts()->latest()->paginate(4)]);
         }else {
             return view('homepage');
         }
