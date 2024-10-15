bcbc3d2ee33239e454943a4e830c4ced01161538
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-10-15 19:34:38 +0500

    add search using laravel/scout backend

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -59,4 +59,11 @@ class PostController extends Controller
 
         return back()->with('success', 'Post successfully updated.');
     }
+
+    public function search($term) {
+        $posts = Post::search($term)->get();
+        $posts->load('user:id,username,avatar');
+
+        return $posts;
+    }
 }
